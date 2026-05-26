5aa6fffd1f68f10cdb52049dc0e39cc5f11b5d94
mohammad-owji <mohammadowji4@gmail.com>
2026-05-26 21:51:35 +0200

    feat: Spring Boot Hauptklasse hinzugefügt

---
+++ backend/src/main/java/com/sophub/SophubApplication.java
@@ -0,0 +1,10 @@
+package com.sophub;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophubApplication{
+    public static void main(String[] args){
+        SpringApplication.run(SophubApplication.class, args);
+    }
+}
